37adb51528e23117899bf8a322f448dfd570fc78
Colton J. McCurdy <mccurdyc22@gmail.com>
2021-11-05 11:14:17 -0400

    client certs
    Signed-off-by: Colton J. McCurdy <mccurdyc22@gmail.com>

--- examples/mtls_certs/main.tf
+++ examples/mtls_certs/main.tf
@@ -64,7 +64,7 @@ resource "tls_cert_request" "server" {
   key_algorithm   = "ECDSA"
   private_key_pem = tls_private_key.server.private_key_pem
 
-  dns_names = ["example.com"]
+  dns_names = ["example.com", "localhost"]
 
   subject {
     common_name         = "example.com"
@@ -98,4 +98,58 @@ resource "tls_locally_signed_cert" "server" {
 resource "local_file" "server_cert_pem" {
   content  = tls_locally_signed_cert.server.cert_pem
   filename = "${path.module}/certs/server_cert.pem"
+}
+
+##########################################################################
+# Client
+##########################################################################
+
+resource "tls_private_key" "client" {
+  algorithm = "ECDSA"
+}
+
+resource "local_file" "client_private_key_pem" {
+  content  = tls_private_key.client.private_key_pem
+  filename = "${path.module}/certs/client.key"
+}
+
+
+resource "tls_cert_request" "client" {
+  key_algorithm   = "ECDSA"
+  private_key_pem = tls_private_key.client.private_key_pem
+
+  dns_names = ["example.com", "localhost"]
+
+  subject {
+    common_name         = "example.com"
+    organization        = "Example Self Signed"
+    country             = "US"
+    organizational_unit = "example.com"
+  }
+}
+
+resource "local_file" "client_csr_pem" {
+  content  = tls_cert_request.client.cert_request_pem
+  filename = "${path.module}/certs/client.csr"
+}
+
+resource "tls_locally_signed_cert" "client" {
+  cert_request_pem   = tls_cert_request.client.cert_request_pem
+  ca_key_algorithm   = "ECDSA"
+  ca_private_key_pem = tls_private_key.ca.private_key_pem
+  ca_cert_pem        = tls_self_signed_cert.ca.cert_pem
+
+  validity_period_hours = 175200 # 20 years
+
+  allowed_uses = [
+    "digital_signature",
+    "key_encipherment",
+    "client_auth",
+    "client_auth",
+  ]
+}
+
+resource "local_file" "client_cert_pem" {
+  content  = tls_locally_signed_cert.client.cert_pem
+  filename = "${path.module}/certs/client_cert.pem"
 }
